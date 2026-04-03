76e34150543ce20f196eae8eb30c2edef75bb3d3
Zach Bai <zachbai2008@gmail.com>
2026-04-02 23:44:56 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import About from "./components/About";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 export default function Home() {
